98d9033f06112365dbf85446c8597b5adc64e30b
yanbo11 <760375443@qq.com>
2022-02-12 09:57:52 +0800

    测试

---
+++ utilcode/src/main/java/com/tomsyb/utilcode/TextUtils.kt
@@ -0,0 +1,14 @@
+package com.tomsyb.utilcode
+
+import android.util.Log
+
+/**
+ * Date: 2022/2/12 9:56
+ * Author: YB
+ * Description:
+ */
+object TextUtils {
+    fun text(){
+        Log.e("text","测试")
+    }
+}
